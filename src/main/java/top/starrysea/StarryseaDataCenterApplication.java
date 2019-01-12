82639fb0b02cc6a79a1cbb3ece9b0ec8830cd814
KumaLoveliver <kuma_loveliver@163.com>
2019-01-12 18:59:52 +0800

    消除StarryseaDataCenterApplication类的warn

--- src/main/java/top/starrysea/StarryseaDataCenterApplication.java
+++ src/main/java/top/starrysea/StarryseaDataCenterApplication.java
@@ -2,7 +2,6 @@ package top.starrysea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StarryseaDataCenterApplication {
