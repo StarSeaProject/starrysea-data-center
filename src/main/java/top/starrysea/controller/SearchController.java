0e8ce69299c71f40bef1a0685cc44c14349d405f
Shuhuai Cao <csh980717@outlook.com>
2019-01-17 17:34:33 +0800

    可查询按日期分割结果

--- src/main/java/top/starrysea/controller/SearchController.java
+++ src/main/java/top/starrysea/controller/SearchController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Mono;
+import top.starrysea.dto.Count;
 import top.starrysea.dto.Most;
 import top.starrysea.service.ISearchService;
+import top.starrysea.vo.CountResource;
 import top.starrysea.vo.MostResource;
 
 @RestController
@@ -20,5 +22,11 @@ public class SearchController {
 	public Mono<MostResource> searchMost(@PathVariable String keyword) {
 		Mono<Most> serviceResult = searchService.searchMostService(keyword);
 		return serviceResult.map(MostResource::of);
+	}
+
+	@GetMapping("/search/count/{type}")
+	public Mono<CountResource> searchCount(@PathVariable String type){
+		Mono<Count> serviceResult = searchService.SearchCountService(type);
+		return serviceResult.map(CountResource::of);
 	}
 }
---
+++ src/main/java/top/starrysea/vo/CountResource.java
@@ -0,0 +1,35 @@
+package top.starrysea.vo;
+
+import top.starrysea.controller.SearchController;
+import top.starrysea.dto.Count;
+import top.starrysea.hateoas.LinkBinding;
+import top.starrysea.hateoas.Resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+public class CountResource extends Resource {
+    private String type;
+    private Map<String, Long> result;
+
+    private CountResource(Count search) {
+        this.type = search.getType();
+        this.result = search.getResult();
+        Map<String, Object> args = new HashMap<>();
+        args.put("type", search.getType());
+        this.addLink(LinkBinding.linkTo(SearchController.class, "searchCount", args));
+    }
+
+    public static CountResource of(Count search) {
+        return new CountResource(search);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Map<String, Long> getResult() {
+        return result;
+    }
+}
