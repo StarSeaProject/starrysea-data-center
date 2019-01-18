f6ecd9d683be6868e8db5ffa0326b420e9b63122
Shuhuai Cao <csh980717@outlook.com>
2019-01-18 21:05:53 +0800

    可从聊天记录中提取更多信息

--- src/main/java/top/starrysea/mapper/DateMapper.java
+++ src/main/java/top/starrysea/mapper/DateMapper.java
@@ -2,9 +2,8 @@ package top.starrysea.mapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.WatchEvent;
+import java.nio.file.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.regex.Pattern;
@@ -13,14 +12,16 @@ import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import top.starrysea.dto.SingleMessage;
 import top.starrysea.mapreduce.Mapper;
 
 public class DateMapper extends Mapper {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	private String fileNameWithoutExtension;
-	private StringBuilder item;
 	private String dateNow = "";
+	private SingleMessage singleMessage;
+	private List<SingleMessage> messages = new ArrayList<>();
 
 	@Override
 	protected void map(WatchEvent<?> event) {
@@ -29,7 +30,6 @@ public class DateMapper extends Mapper {
 
 	private void split(String fileName) {
 		this.fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-		item = new StringBuilder();
 		File file = new File(inputPath, fileName);
 		try (Stream<String> stream = Files.lines(file.toPath())) {
 			stream.forEach(s -> execStr(s.replace("\ufeff", "")));
@@ -42,45 +42,48 @@ public class DateMapper extends Mapper {
 	}
 
 	private void execStr(String str) {
-		String strToSend;
 		String pattern = "\\d{4}-\\d{2}-\\d{2} \\d{1,2}:\\d{2}:\\d{2} .+([<(]).+([>)])";
 		// 用于判断单个群聊聊天记录开头(日期,昵称,QQ号或邮箱)的正则表达式
 		if (str == null)
 			return;
 		if (Pattern.matches(pattern, str)) {
-			if (dateNow.equals(str.substring(0, 10))) {
-				item.append(str);
-				item.append("\n");
-			} else {
+		    if(singleMessage != null){
+				messages.add(singleMessage);
+			}
+		    singleMessage = new SingleMessage();
+		    singleMessage.setHead(str + "\n");
+			if (!dateNow.equals(str.substring(0, 10))) {
 				dateNow = str.substring(0, 10);
 				// 当日期与前一天不一样时才写入
-				strToSend = item.toString();
-				saveStr(strToSend);
-				item = new StringBuilder();
-				item.append(str);
-				item.append("\n");
+				save(messages);
+				messages.clear();
 			}
 		} else {
-			item.append(str);
-			item.append("\n");
+			if (singleMessage.getBody() == null) {
+				singleMessage.setBody(str + "\n");
+			} else {
+				singleMessage.setBody(singleMessage.getBody() + str + "\n");
+			}
 		}
 	}
 
 	private void execStr() {
+	    if(singleMessage==null)
+	        return;
+	    messages.add(singleMessage);
+	    save(messages);
+	    messages.clear();
 		// 将最后一天的聊天记录送出
-		String strToSend;
-		strToSend = item.toString();
-		saveStr(strToSend);
 	}
 
-	private void saveStr(String str) {
+	private void save(List<SingleMessage> messages){
 		File fileToWrite;
 		File directory;
-		if (str.equals(""))
+		if(messages==null||messages.size()==0)
 			return;
-		String year = str.substring(0, 4);
-		String month = str.substring(5, 7);
-		String date = str.substring(0, 10);
+		String date = messages.get(0).getDate();
+		String year = date.substring(0, 4);
+		String month = date.substring(5, 7);
 		String strDirectory = outputPath + "/" + fileNameWithoutExtension + "/" + year + "/" + month;
 		String strFile = strDirectory + "/" + date + ".txt";
 		directory = new File(strDirectory);
@@ -99,8 +102,17 @@ public class DateMapper extends Mapper {
 				logger.error(e.getMessage(), e);
 			}
 		}
+		StringBuilder text = new StringBuilder();
+		messages.forEach(m -> {
+			text.append(m.getHead());
+			text.append(m.getBody());
+		});
+		saveText(text.toString(),strFile);
+	}
+
+	private void saveText(String text, String path){
 		try {
-			Files.write(Paths.get(strFile), str.getBytes());
+			Files.write(Paths.get(path),text.getBytes());
 		} catch (IOException e) {
 			logger.error(e.getMessage(), e);
 		}
