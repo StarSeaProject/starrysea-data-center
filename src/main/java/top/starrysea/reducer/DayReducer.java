5feea175b3c5f72669d2baf60d68e9d589f10544
Shuhuai Cao <csh980717@outlook.com>
2019-01-19 22:30:58 +0800

    可按年统计和按天统计,DateReducer重命名为MonthReducer

---
+++ src/main/java/top/starrysea/reducer/DayReducer.java
@@ -0,0 +1,97 @@
+package top.starrysea.reducer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.starrysea.mapreduce.Reducer;
+import top.starrysea.repository.CountRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.regex.Pattern;
+
+public class DayReducer extends Reducer {
+    private Map<String, Long> chatCount;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private CountRepository countRepository;
+
+    @Override
+    protected void reduce() {
+        chatCount = new ConcurrentHashMap<>();
+        String fileNameWithoutExtension = getFileName().substring(0, getFileName().lastIndexOf('.'));
+        analyze(inputPath + "/" + fileNameWithoutExtension);
+    }
+
+    private void analyze(String fileDirectory) {
+        List<File> files = new ArrayList<>();
+        File rootDir = new File(fileDirectory);
+        File[] years = rootDir.listFiles();
+        for (File i : years) {
+            if (i.isDirectory()) {
+                File[] months = i.listFiles();
+                for (File j : months) {// 获取所有月份的目录
+                    if (j.isDirectory()) {
+                        File[] days = j.listFiles();
+                        for (File k : days) {
+                            if (k.isFile()) {
+                                files.add(k);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        CountDownLatch countDownLatch = new CountDownLatch(files.size());
+        files.forEach(f -> managerThreadPool.apply(new ChatCount(f, countDownLatch)));
+        try {
+            countDownLatch.await();
+            logger.info("对每日发言数的分析结束.");
+            logger.info("共有{}天.", chatCount.size());
+            countRepository.findById("day").subscribe(chatCountTemp -> {
+                chatCountTemp.getResult().putAll(chatCount);
+                chatCountTemp.setResult(chatCount);
+                countRepository.save(chatCountTemp).subscribe();
+                logger.info("每日分析已存入数据库.");
+            });
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private class ChatCount implements Runnable {
+        private File path;
+        private String pattern = "\\d{4}-\\d{2}-\\d{2} \\d{1,2}:\\d{2}:\\d{2} .+([<(]).+([>)])";
+        private CountDownLatch countDownLatch;
+
+        ChatCount(File path, CountDownLatch countDownLatch) {
+            this.path = path;
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            String filePath = path.toString();
+            String date = filePath.substring(filePath.length() - 14, filePath.length() - 4);
+            long count = 0;
+            try {
+                count = Files.lines(path.toPath()).map(s -> s.replace("\ufeff", ""))
+                        .filter(s -> Pattern.matches(pattern, s)).count();
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+            chatCount.put(date, count);
+            countDownLatch.countDown();
+        }
+    }
+
+    public Reducer setCountRepository(CountRepository countRepository) {
+        this.countRepository = countRepository;
+        return this;
+    }
+}
---
+++ src/main/java/top/starrysea/reducer/YearReducer.java
@@ -0,0 +1,104 @@
+package top.starrysea.reducer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import top.starrysea.mapreduce.Reducer;
+import top.starrysea.repository.CountRepository;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.regex.Pattern;
+
+public class YearReducer extends Reducer {
+    private Map<String, Long> chatCount;
+    private ThreadPoolTaskExecutor threadPool = new ThreadPoolTaskExecutor();
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private CountRepository countRepository;
+
+    @Override
+    protected void reduce() {
+        chatCount = new ConcurrentHashMap<>();
+        String fileNameWithoutExtension = getFileName().substring(0, getFileName().lastIndexOf('.'));
+        analyze(inputPath + "/" + fileNameWithoutExtension);
+    }
+
+    private void analyze(String fileDirectory) {
+        List<File> files = new ArrayList<>();
+        File rootDir = new File(fileDirectory);
+        File[] years = rootDir.listFiles();
+        for (File i : years) {
+            if (i.isDirectory()) {
+                files.add(i);
+            }
+        }
+        CountDownLatch countDownLatch = new CountDownLatch(files.size());
+        files.forEach(f -> managerThreadPool.apply(new ChatCount(f, countDownLatch)));
+        try {
+            countDownLatch.await();
+            logger.info("对每年发言数的分析结束.");
+            logger.info("共有{}年.", chatCount.size());
+            countRepository.findById("year").subscribe(chatCountTemp -> {
+                chatCountTemp.getResult().putAll(chatCount);
+                chatCountTemp.setResult(chatCount);
+                countRepository.save(chatCountTemp).subscribe();
+                logger.info("每年分析已存入数据库.");
+            });
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+
+    }
+}
+
+    private class ChatCount implements Runnable {
+        private File dirPath;
+        private String pattern = "\\d{4}-\\d{2}-\\d{2} \\d{1,2}:\\d{2}:\\d{2} .+([<(]).+([>)])";
+        private CountDownLatch countDownLatch;
+
+        ChatCount(File dirPath, CountDownLatch countDownLatch) {
+            this.dirPath = dirPath;
+            this.countDownLatch = countDownLatch;
+        }
+
+        @Override
+        public void run() {
+            String dir = dirPath.toString();
+            String date = dir.substring(dir.length() - 4);
+            File[] months = dirPath.listFiles();
+            List<File> itemsArrayList = new ArrayList<>();
+            for (File i : months) {
+                if (i.isDirectory()) {
+                    File[] days = i.listFiles();
+                    for (File j : days) {
+                        if (j.isFile()) {
+                            itemsArrayList.add(j);
+                        }
+                    }
+                }
+            }
+            long count = itemsArrayList.stream().mapToLong(f -> {
+                try {
+                    return Files.lines(f.toPath()).map(s -> s.replace("\ufeff", ""))
+                            .filter(s -> Pattern.matches(pattern, s)).count();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                    return 0;
+                }
+            }).sum();
+            chatCount.put(date, count);
+            countDownLatch.countDown();
+        }
+    }
+
+    public Reducer setCountRepository(CountRepository countRepository) {
+        this.countRepository = countRepository;
+        return this;
+    }
+}
