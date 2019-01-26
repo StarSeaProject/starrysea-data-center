2f9e250f7738ec25c94ce437159da295fb328dd7
kuma_loveliver <kuma_loveliver@163.com>
2019-01-26 16:08:13 +0800

    添加redis的支持

--- src/main/java/top/starrysea/StarryseaDataCenterApplication.java
+++ src/main/java/top/starrysea/StarryseaDataCenterApplication.java
@@ -2,12 +2,30 @@ package top.starrysea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 public class StarryseaDataCenterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarryseaDataCenterApplication.class, args);
+	}
+
+	@Bean
+	public ReactiveRedisTemplate<String, Object> getReactiveRedisTemplate(
+			ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
+		RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+		RedisSerializer<Object> jsonSerializer = new GenericJackson2JsonRedisSerializer();
+		RedisSerializationContext<String, Object> serializationContext = RedisSerializationContext
+				.<String, Object>newSerializationContext().key(stringSerializer).value(jsonSerializer)
+				.hashKey(jsonSerializer).hashValue(jsonSerializer).build();
+		return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, serializationContext);
 	}
 
 }
