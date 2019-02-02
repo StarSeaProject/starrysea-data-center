14d618dea12370a08b6efa055634dd467086f137
kuma_loveliver <kuma_loveliver@163.com>
2019-02-02 09:16:35 +0800

    解决mongodb的key不能带.的bug

--- src/main/java/top/starrysea/StarryseaDataCenterApplication.java
+++ src/main/java/top/starrysea/StarryseaDataCenterApplication.java
@@ -1,8 +1,12 @@
 package top.starrysea;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -28,4 +32,11 @@ public class StarryseaDataCenterApplication {
 		return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory, serializationContext);
 	}
 
+	@Autowired
+	private MappingMongoConverter mappingMongoConverter;
+
+	@PostConstruct
+	public void initMappingMongoConverter() {
+		mappingMongoConverter.setMapKeyDotReplacement("\\+");
+	}
 }
