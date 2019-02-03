881be153c2278570605781e0adab90743ebf5c5a
KumaLoveliver <kuma_loveliver@163.com>
2019-02-03 19:17:11 +0800

    放开reactiveRedisTemplate的访问权限给子类

--- src/main/java/top/starrysea/StarryseaDataCenterApplication.java
+++ src/main/java/top/starrysea/StarryseaDataCenterApplication.java
@@ -5,7 +5,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 
 @SpringBootApplication
